570b09969f4b975a7e80463c9f9899dfdac51c1d
Corrado <corradoct@hotmail.it>
2020-11-12 00:40:30 +0100

    fix2

--- resources/views/upr/apartments/show.blade.php
+++ resources/views/upr/apartments/show.blade.php
@@ -181,18 +181,18 @@
   </script>
 
   <?php
-   $handle = fopen("counter.txt", "r");
-   if(!$handle){
-     echo "could not open the file";
-    } else {
-      $counter = ( int ) fread ($handle,20);
-      fclose ($handle);
-      $counter++;
-      $handle = fopen("counter.txt", "w" );
-      fwrite($handle,$counter);
-      fclose ($handle);
-      json_encode($counter);
-    }
+   // $handle = fopen("counter.txt", "r");
+   // if(!$handle){
+   //   echo "could not open the file";
+   //  } else {
+   //    $counter = ( int ) fread ($handle,20);
+   //    fclose ($handle);
+   //    $counter++;
+   //    $handle = fopen("counter.txt", "w" );
+   //    fwrite($handle,$counter);
+   //    fclose ($handle);
+   //    json_encode($counter);
+   //  }
   ?>
 
   <script>
